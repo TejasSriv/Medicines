e5bfd651cf6aad9859486054e1bfa463a067c0ca
TejasSriv <tejas.sriv.44@gmail.com>
2024-11-13 15:30:05 +0530

    Added Medicine interface and functions to manipulate data in the storage

--- types.ts
+++ types.ts
@@ -1,6 +1,28 @@
+import { RouteProp } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+
 export type RootStackParamList = {
     Home: undefined;
     AddMedicine: undefined;
     MedicineList: undefined;
+    MedicationDetails: { medicineName: string }; // Add parameters if needed
   };
-  
+
+export type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
+export type AddMedicineScreenNavigationProp = StackNavigationProp<RootStackParamList,'AddMedicine'>;
+
+export interface Medicine {
+  id: string; // unique ID for each medicine
+  name: string;
+  dosage: string;
+  form: string;
+}
+
+export type MedicationDetailsScreenRouteProp = RouteProp<
+    RootStackParamList, 'MedicationDetails'
+>;
+
+export type MedicationDetailsScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'MedicationDetails'
+>;
