8506d12fa4b3e8412d27ce17ab1f0c6a8e0a61b8
TejasSriv <tejas.sriv.44@gmail.com>
2024-11-13 15:33:20 +0530

    Updated StorageService to handle the medicine object

--- services/StorageService.ts
+++ services/StorageService.ts
@@ -1,48 +1,34 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Medicine } from '../types';
 
-const MEDICINE_KEY = 'medicines';
+// const MEDICINE_KEY = 'MEDICINES_KEY';
 
-//function to add a medicine to the list
-export async function addMedicine(medicineName: string) {
-  try {
-    const existingMedicines = await getMedicines();
-    const updatedMedicines = [...existingMedicines, medicineName];
-    await AsyncStorage.setItem(MEDICINE_KEY, JSON.stringify(updatedMedicines));
-  } catch (error) {
-    console.error('Error adding medicines: ', error);
+class StorageService{
+  static async addMedicine(medicine: Medicine): Promise<void> {
+    try{
+      const storedMedicines = await this.getMedicines();
+      storedMedicines.push(medicine);
+      await AsyncStorage.setItem('medicines', JSON.stringify(storedMedicines));
+    } catch(error) {
+      console.error('Error saving medicines:', error);
+    }
   }
+  static async getMedicines(): Promise<Medicine[]> {
+    try{
+      const storedMedicines = await AsyncStorage.getItem('medicines');
+      return storedMedicines ? JSON.parse(storedMedicines) : [];
+    } catch(error) {
+      console.error('Error retrieving medicines:', error);
+      return [];
+    }
+  }
+  static async clearMedicines(): Promise<void> {
+    try{
+      await AsyncStorage.removeItem('medicines');
+    } catch (error) {
+      console.error('Error clearing medicines:', error);
+    }
+  } 
 }
 
-//function to retrieve all medicines
-async function getMedicines(): Promise<string[]> {
-  try {
-    const storedMedicines = await AsyncStorage.getItem(MEDICINE_KEY);
-    return storedMedicines ? JSON.parse(storedMedicines) : [];
-  } catch (error) {
-    console.error('Error retrieving medicines:', error);
-    return [];
-  }
-}
-export default getMedicines;
-
-//fucntion to retireve a single medicine by its name
-export async function getMedicineByName({ medicineName }: { medicineName: string; }): Promise<string | null> {
-  try {
-    const storedMedicines = await AsyncStorage.getItem(MEDICINE_KEY);
-    const medicines = storedMedicines ? JSON.parse(storedMedicines) : [];
-    const medicine = medicines.find((name: string) => name.toLowerCase() === medicineName.toLowerCase());
-    return medicine || null;
-  } catch (error) {
-    console.error('Error retrieving single medicine:', error);
-    return null;
-  }
-}
-
-//function to clear all medicines
-export async function clearMedicines(): Promise<void> {
-  try {
-    await AsyncStorage.removeItem(MEDICINE_KEY);
-  } catch (error) {
-    console.error('Error clearing medicines:', error);
-  }
-}
+export default StorageService;
